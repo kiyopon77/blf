06030befa498fd6d3c9ad458a71b93e8428d468a
harshgit1406 <harsh14raj1111@gmail.com>
2026-03-23 19:51:44 +0530

    changes backend api logic as per new documnet db stucture

--- backend/app/schemas/document.py
+++ backend/app/schemas/document.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
+from app.models.document import EntityType
 
 class DocumentResponse(BaseModel):
     document_id: int
     label: str
     file_name: str
     file_type: str
-    entity_type: str
-    entity_id: int
+    entity: EntityType
+    sale_id: int
     uploaded_at: datetime
 
     class Config:
