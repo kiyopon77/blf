4f01832c5cc386701043cb8d07b00ea287568c3c
sire <sire.saini27@gmail.com>
2026-03-30 21:58:23 +0530

    added extra functions to button

--- frontend/src/components/ui/AdminButton.tsx
+++ frontend/src/components/ui/AdminButton.tsx
@@ -4,15 +4,21 @@ const AdminButton = ({
   children,
   onClick,
   icon,
+  type = "button",
+  disabled = false,
 }: {
   children: ReactNode
   onClick?: () => void
   icon?: ReactNode
+  type?: "button" | "submit"
+  disabled?: boolean
 }) => {
   return (
     <button
+      type={type}
       onClick={onClick}
-      className="mt-4 hover:cursor-pointer sm:mt-0 flex items-center space-x-2 border border-[#D4A22A] text-[#D4A22A] hover:bg-[#D4A22A] hover:text-white px-5 py-2.5 rounded text-sm font-medium transition-colors duration-200"
+      disabled={disabled}
+      className="mt-4 hover:cursor-pointer sm:mt-0 flex items-center space-x-2 border border-[#D4A22A] text-[#D4A22A] hover:bg-[#D4A22A] hover:text-white px-5 py-2.5 rounded text-sm font-medium transition-colors duration-200 disabled:opacity-50"
     >
       {icon}
       <span>{children}</span>
