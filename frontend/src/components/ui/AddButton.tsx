f2af899c953467183c5ee6f9322b1615f788a6cc
sire <sire.saini27@gmail.com>
2026-03-31 19:25:54 +0530

    modified props

--- frontend/src/components/ui/AddButton.tsx
+++ frontend/src/components/ui/AddButton.tsx
@@ -4,14 +4,17 @@ const AddButton = ({
   children,
   onClick,
   icon,
+  disabled = false,
 }: {
   children: ReactNode
   onClick?: () => void
   icon?: ReactNode
+  disabled?: boolean
 }) => {
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className="mt-4 hover:cursor-pointer sm:mt-0 flex items-center space-x-2 border border-green-500 text-green-600 hover:bg-green-500 hover:text-white px-5 py-2.5 rounded text-sm font-medium transition-colors duration-200"
     >
       {icon}
