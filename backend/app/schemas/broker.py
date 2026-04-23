7e87f73e6a58e9abaf90dc807f210117a343e706
harshgit1406 <harsh14raj1111@gmail.com>
2026-04-23 20:40:42 +0530

    updated broker api points

--- backend/app/schemas/broker.py
+++ backend/app/schemas/broker.py
@@ -1,21 +1,43 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
+from enum import Enum
+
+
+class KYCStatus(str, Enum):
+    PENDING = "PENDING"
+    DONE = "DONE"
+
 
 class BrokerBase(BaseModel):
     society_id: int
     broker_name: Optional[str] = None
     phone: Optional[str] = None
+
+    # NEW
+    company_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    kyc_status: Optional[KYCStatus] = KYCStatus.PENDING
+
     user_id: int
+
 
 class BrokerCreate(BrokerBase):
     pass
 
+
 class BrokerUpdate(BaseModel):
     broker_name: Optional[str] = None
     phone: Optional[str] = None
+
+    # NEW
+    company_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    kyc_status: Optional[KYCStatus] = None
+
     user_id: Optional[int] = None
     society_id: Optional[int] = None
+
 
 class BrokerResponse(BrokerBase):
     broker_id: int
